63b9146f545c294f0d60aa72487104987d3e4f75
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-11-06 19:58:29 +0900

    Set: Story 앱 url, view 세팅 완료

--- story/urls.py
+++ story/urls.py
@@ -2,4 +2,16 @@ from django.urls import path
 from story import views
 
 urlpatterns = [
+    path('create/',views.StoryCreateView.as_view(), name='story_create_view'),
+    path('',views.MainPageView.as_view(), name='main_page_view'),
+    path('<int:story_id>/',views.DetailPageView.as_view(), name='detail_page_view'),
+    path('delete/<int:story_id>/',views.StoryDeleteView.as_view(), name='story_delete_view'),
+
+    path('<int:story_id>/like/',views.LikeView.as_view(), name='like_view'),
+    path('<int:story_id>/hate/',views.HateView.as_view(), name='hate_view'),
+    path('<int:story_id>/bookmark/',views.BookmarkView.as_view(), name='bookmark_view'),
+    
+    path('<int:story_id>/comment/create/',views.CommentView.as_view(), name='comment_create_view'),
+    path('<int:story_id>/comment/',views.CommentView.as_view(), name='comment_view'),
+    path('<int:story_id>/comment/delete/<int:comment_id>/',views.CommentView.as_view(), name='comment_delete_view'),
 ]
--- story/views.py
+++ story/views.py
@@ -1,2 +1,56 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
+
+class MainPageView(APIView):
+    def get(self, request):
+        """메인페이지입니다."""
+        pass
+  
+    
+class DetailPageView(APIView):
+    def get(self, request, story_id):
+        """작성된 게시글(동화) 상세페이지입니다."""
+        pass
+  
+  
+class StoryCreateView(APIView):
+    def post(self, request):
+        """게시글(동화) 작성 페이지입니다."""
+        pass
+
+class StoryDeleteView(APIView):
+    def delete(self, request, story_id):
+        """작성된 게시글(동화)을 삭제하는 기능입니다."""
+        pass
+
+
+class LikeView(APIView):
+    def post(self, request, story_id):
+        """게시글 좋아요 기능입니다."""
+        pass
+    
+    
+class HateView(APIView):
+    def post(self, request, story_id):
+        """게시글 싫어요 기능입니다."""
+        pass
+    
+    
+class BookmarkView(APIView):
+    def post(self, request, story_id):
+        """관심있는 게시글(동화)을 북마크합니다."""
+        pass
+
+
+class CommentView(APIView):
+    def get(self, request, story_id):
+        """댓글을 조회합니다."""
+        pass
+    
+    def post(self, request, story_id):
+        """댓글을 작성합니다."""
+        pass
+    
+    def delete(self, request, story_id, comment_id):
+        """댓글을 삭제합니다."""
+        pass
