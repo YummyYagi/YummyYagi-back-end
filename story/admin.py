b1e19d2aeac0c2dc84541a8e1106544da609cd88
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-11-06 19:41:57 +0900

    Add: admin.py에 생성된 Story앱 모델들 추가

--- story/admin.py
+++ story/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from story.models import Story, Content, Comment
 
-
+admin.site.register(Story)
+admin.site.register(Content)
+admin.site.register(Comment)
--- story/models.py
+++ story/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.conf import settings
 
+class Story(models.Model):
+    """
+    게시글(스토리)을 정의하는 클래스입니다.
+    
+    - author : 스토리 작성자입니다.
+        - 로그인 한 사용자를 자동으로 지정합니다.
+    - title : 스토리 제목입니다.
+    - like : 스토리를 좋아요 한 사용자와의 관계입니다.
+    - hate : 스토리를 싫어요 한 사용자와의 관계입니다.
+    - bookmark : 스토리를 북마크 한 사용자와의 관계입니다.
+    """
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="작성자", on_delete=models.CASCADE)
+    title = models.CharField("스토리 제목", max_length=255)
+    like = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name="좋아요", related_name='like_stories', blank=True)
+    hate = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name="싫어요", related_name='hate_stories', blank=True)
+    bookmark = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name="북마크", related_name='bookmark_stories', blank=True)
 
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        db_table = 'story'
+        verbose_name_plural = 'stories'
+        
+
+def story_image_upload_path(instance, filename):
+    """게시글(스토리) 이미지의 저장 경로를 정의합니다."""
+    return f'story/{instance.story.id}/{filename}'
+
+class Content(models.Model):
+    """
+    게시글(스토리)의 내용을 정의하는 클래스입니다.
+    
+    - story : 게시글입니다.
+    - paragraph : 게시글 내용의 문단입니다.
+    - image : 해당 문단의 이미지입니다.
+    
+    """
+    story = models.ForeignKey(Story, verbose_name="작성자", on_delete=models.CASCADE, related_name="contents")
+    paragraph = models.TextField("문단")
+    image = models.ImageField("문단 이미지", upload_to=story_image_upload_path)
+    
+    class Meta:
+        db_table = 'content'
+    
+    
+class Comment(models.Model):
+    """
+    게시글(스토리)의 댓글을 정의하는 클래스입니다.
+    
+    - author : 댓글 작성자입니다.
+        - 로그인 한 사용자를 자동으로 지정합니다.
+    - story : 댓글이 작성된 게시글(스토리)입니다.  
+    - content : 댓글의 내용입니다.
+    """
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="작성자", on_delete=models.CASCADE)
+    story = models.ForeignKey(Story, verbose_name="스토리", on_delete=models.CASCADE)
+    content = models.TextField("댓글 내용")
+    
+    class Meta:
+        db_table = 'comment'
