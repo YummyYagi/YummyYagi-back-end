39c4e0797342fbbef14a9dffb720ed210e83b530
USER\HOME <leekh9997@naver.com>
2023-11-07 12:23:46 +0900

    Feat: 로그인 기능 추가 & Fix: UserSerializer fields 변경 & Remove: 중복체크 class view 삭제

--- user/serializers.py
+++ user/serializers.py
@@ -1,11 +1,13 @@
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 from user.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
+# 회원가입
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ['email', 'password', 'country', 'nickname', 'profile_img']
 
     def create(self, validated_data):
         password = validated_data.pop('password')
@@ -13,3 +15,34 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+
+# 로그인
+class LoginSerializer(TokenObtainPairSerializer):
+    
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+    def validate(self, request_data):
+        request_email = request_data.get('email')
+        request_password = request_data.get('password')
+
+        try:
+            user = User.objects.get(email=request_email)
+        except User.DoesNotExist:
+            raise exceptions.NotFound({'status':'404', 'error':'사용자를 찾을 수 없습니다. 로그인 정보를 확인하세요.'})
+        
+        if not user.check_password(request_password):
+            raise exceptions.AuthenticationFailed({'status':'401', 'error':'비밀번호가 일치하지 않습니다.'})
+        else:
+            data = super().validate(request_data)
+            return data
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        token['nickname'] = user.nickname
+        token['profile_img'] = user.profile_img.url
+
+        return token
