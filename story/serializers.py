3820244dfab861f26b8a14964d709e0082f3eb1d
myeonghee2 <mang1670@gmail.com>
2023-11-08 14:38:05 +0900

    Feat: content,storylist serializer 추가, 메인페이지 view 추가

---
+++ story/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from story.models import Story, Content
+
+
+class ContentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Content
+        fields = '__all__'
+
+
+class StoryListSerializer(serializers.ModelSerializer):
+    story_id = serializers.CharField(source='id')
+    author_id = serializers.CharField(source='author')
+    author_nickname = serializers.CharField(source='author.nickname')
+    story_title = serializers.CharField(source='title')
+
+    def get_first_content(self, obj):
+        first_content = obj.contents.first()
+        if first_content:
+            return {
+                'story_first_paragraph': first_content.paragraph,
+                'story_image': first_content.image.url,
+            }
+        return None
+
+    content = serializers.SerializerMethodField(method_name='get_first_content')
+
+    class Meta:
+        model = Story
+        fields = ('story_id', 'author_id', 'author_nickname', 'story_title', 'content')
