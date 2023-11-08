32bf266607b38ae3de2430343941e9fceea286c7
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-11-08 14:55:32 +0900

    Refactor: RESTful 디자인을 따르도록 URL과 뷰 구조 변경

--- story/views.py
+++ story/views.py
@@ -5,26 +5,23 @@ from story.serializers import StoryListSerializer
 from rest_framework import status
 
 
-class MainPageView(APIView):
-    def get(self, request):
-        """메인페이지입니다."""
-        stories = Story.objects.all()
-        serializer = StoryListSerializer(stories, many=True)
-        return Response({'status':'200', 'story_list':serializer.data}, status=status.HTTP_200_OK)
-  
-    
-class DetailPageView(APIView):
-    def get(self, request, story_id):
-        """작성된 게시글(동화) 상세페이지입니다."""
-        pass
-  
-  
-class StoryCreateView(APIView):
+class StoryView(APIView):
+    def get(self, request, story_id = None):
+        """
+        story_id가 없을 경우 모든 계시물을 Response 합니다.
+        story_id가 있을 경우 특정 게시물을 Response 합니다.
+        """
+        if story_id is None:
+            stories = Story.objects.all()
+            serializer = StoryListSerializer(stories, many=True)
+            return Response({'status':'200', 'story_list':serializer.data}, status=status.HTTP_200_OK)
+        else:
+            """상세 페이지"""
+
     def post(self, request):
         """게시글(동화) 작성 페이지입니다."""
         pass
-
-class StoryDeleteView(APIView):
+    
     def delete(self, request, story_id):
         """작성된 게시글(동화)을 삭제하는 기능입니다."""
         pass
--- user/urls.py
+++ user/urls.py
@@ -6,9 +6,6 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login_view'),
     path('mypage/', views.MyPageView.as_view(), name='mypage_view'),
     path('info/', views.UserInfoView.as_view(), name='user_info_view'),
-    path('info-update/', views.UserInfoView.as_view(), name='user_info_update_view'),
-    path('pwd-update/', views.UserInfoView.as_view(), name='user_pwd_update_view'),
-    path('delete/', views.UserInfoView.as_view(), name='user_info_delete_view'),
     path('qna/', views.QnaView.as_view(), name='qna_view'),
     path('pwd-reset/', views.PasswordResetView.as_view(), name='password_reset_view'),
 ]
