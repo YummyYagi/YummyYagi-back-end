ce50adba2e0e00a009c3a391fd39c7757db2a7ca
USER\HOME <leekh9997@naver.com>
2023-11-29 11:42:11 +0900

    Fix: 소셜로그인 BASE_URL 통합

--- user/views.py
+++ user/views.py
@@ -76,12 +76,9 @@ class LoginView(TokenObtainPairView):
     serializer_class = LoginSerializer
 
 
-    
-KAKAO_BASE_URL = "http://127.0.0.1:5501/"
-GOOGLE_BASE_URL = "http://127.0.0.1:5501/"
-NAVER_BASE_URL = "http://127.0.0.1:5501/"
-
+BASE_URL = "http://127.0.0.1:5501/"
 STATE = secrets.token_urlsafe(16)
+
 
 class SocialUrlView(APIView):
     def post(self,request):
@@ -89,15 +86,15 @@ class SocialUrlView(APIView):
         if social is None:
             return Response({'error':'소셜로그인이 아닙니다'},status=status.HTTP_400_BAD_REQUEST)
         elif social == 'kakao':
-            url = 'https://kauth.kakao.com/oauth/authorize?client_id=' + settings.KAKAO_REST_API_KEY + '&redirect_uri=' + KAKAO_BASE_URL + '&response_type=code&prompt=login'
+            url = 'https://kauth.kakao.com/oauth/authorize?client_id=' + settings.KAKAO_REST_API_KEY + '&redirect_uri=' + BASE_URL + '&response_type=code&prompt=login'
             return Response({'url':url},status=status.HTTP_200_OK)
         elif social == 'google':
             client_id = settings.SOCIAL_AUTH_GOOGLE_CLIENT_ID
-            redirect_uri = GOOGLE_BASE_URL 
+            redirect_uri = BASE_URL 
             url = f"https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=email%20profile"
             return Response({'url': url}, status=status.HTTP_200_OK)
         elif social == 'naver':
-            url = "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id="+ settings.SOCIAL_AUTH_NAVER_CLIENT_ID + "&redirect_uri=" + NAVER_BASE_URL + "&state=" + STATE
+            url = "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id="+ settings.SOCIAL_AUTH_NAVER_CLIENT_ID + "&redirect_uri=" + BASE_URL + "&state=" + STATE
             return Response({'url':url},status=status.HTTP_200_OK) 
 
 
@@ -109,7 +106,7 @@ class KakaoLoginView(APIView):
             data={
                 "grant_type":"authorization_code",
                 "client_id": settings.KAKAO_REST_API_KEY,
-                "redirect_uri":KAKAO_BASE_URL,
+                "redirect_uri":BASE_URL,
                 "code":code,
                 'client_secret': settings.KAKAO_SECRET_KEY,
             },
@@ -179,7 +176,7 @@ class NaverLoginView(APIView):
         code = request.data.get('code')
         client_id = settings.SOCIAL_AUTH_NAVER_CLIENT_ID
         client_secret = settings.SOCIAL_AUTH_NAVER_SECRET
-        redirect_uri = NAVER_BASE_URL
+        redirect_uri = BASE_URL
         
         # 네이버 API로 액세스 토큰 요청
         access_token_request = requests.post("https://nid.naver.com/oauth2.0/token",
@@ -262,7 +259,7 @@ class GoogleLoginView(APIView):
 
         client_id = settings.SOCIAL_AUTH_GOOGLE_CLIENT_ID
         client_secret = settings.SOCIAL_AUTH_GOOGLE_CLIENT_SECRET
-        redirect_uri = GOOGLE_BASE_URL
+        redirect_uri = BASE_URL
 
         #  구글 API로 액세스 토큰 요청
         access_token_request = requests.post("https://oauth2.googleapis.com/token",
