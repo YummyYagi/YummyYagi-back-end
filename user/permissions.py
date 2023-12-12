e9cc1d6ca3a6e00cc9006856f289f9a4da42d004
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-12-13 00:52:43 +0900

    Remove: permission_classes 중 IsOwner은 필요없어서 삭제

--- user/permissions.py
+++ user/permissions.py
@@ -2,15 +2,6 @@ from rest_framework import permissions
 from rest_framework.exceptions import PermissionDenied, AuthenticationFailed
 
 
-class IsOwner(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        if obj.owner == request.user:
-            return True
-        else:
-            raise PermissionDenied({"status": "403", "error": "접근 권한이 없습니다."})
-
-
 class IsAuthenticated(permissions.BasePermission):
     def has_permission(self, request, view):
         if not request.user or not request.user.is_authenticated:
